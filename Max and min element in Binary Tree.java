573e85755df0f926043e66dca7cd212184e8af60
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-09-15 22:47:14 +0530

    Create  Max and min element in Binary Tree.java

---
+++ Max and min element in Binary Tree.java
@@ -0,0 +1,55 @@
+//User function Template for Java
+
+/*
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+class Solution{
+    public static int findMax(Node root){
+        //code here
+        if(root==null){
+            return 0;
+        }
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        int result=root.data;
+        while(!q.isEmpty()){
+            Node temp=q.poll();
+            result=Math.max(result,temp.data);
+            if(temp.left!=null){
+                q.add(temp.left);
+            }
+            if(temp.right!=null){
+                q.add(temp.right);
+            }
+        }
+        return result;
+        
+    }
+    public static int findMin(Node root){
+        //code here
+         if(root==null){
+            return 0;
+        }
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        int result=root.data;
+        while(!q.isEmpty()){
+            Node temp=q.poll();
+            result=Math.min(result,temp.data);
+            if(temp.left!=null){
+                q.add(temp.left);
+            }
+            if(temp.right!=null){
+                q.add(temp.right);
+            }
+        }
+        return result;
+    }
+}
