e36e2eb3ff666e0daffdfaf7c12408d74a195430
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-09-16 21:25:29 +0530

    Create Floor in BST.java

---
+++ Floor in BST.java
@@ -0,0 +1,26 @@
+// User function Template for Java
+
+class Solution {
+    public static int floor(Node root, int x) {
+        // Code here
+        Node temp=null;
+        if(root==null){
+            return 0;
+        }
+        while(root!=null){
+            if(root.data==x){
+                return x;
+            }else if(root.data>x){
+                root=root.left;
+            }else{
+                temp=root;
+                root=root.right;
+            }
+        }
+        if(temp==null){
+            return -1;
+        }else{
+            return temp.data;
+        }
+    }
+}
