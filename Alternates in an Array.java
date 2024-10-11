93ef58cbd7a9c18d324ec009e1773a9ba235a362
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-10-11 21:15:14 +0530

    Create Alternates in an Array.java

---
+++ Alternates in an Array.java
@@ -0,0 +1,14 @@
+// User function Template for Java
+
+// arr[] is the array
+// n is the number of elements in array.
+class Solution {
+    public static void print(int arr[]) {
+        // your code here
+        for(int i=0;i<arr.length;i++){
+            if(i%2==0){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}
