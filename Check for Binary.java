7dc38bf3f48259931999e8ef8d819e18cbc07f64
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-10-26 21:38:55 +0530

    Create Check for Binary.java

---
+++ Check for Binary.java
@@ -0,0 +1,12 @@
+
+class GfG
+{
+	boolean isBinary(String str)
+	{
+	  //Your code here
+	  for(int i=0;i<str.length();i++){
+          if(str.charAt(i)!='0' && str.charAt(i)!='1') return false;
+        }
+        return true;
+	}
+}
