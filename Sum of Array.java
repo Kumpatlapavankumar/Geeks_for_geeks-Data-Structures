f5f11340fd97541080ed3acd1b0955a74ce9e3b9
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-11-06 21:59:29 +0530

    Create Sum of Array.java

---
+++ Sum of Array.java
@@ -0,0 +1,13 @@
+
+// User function Template for Java
+
+class Solution {
+    int sum(int arr[]) {
+        // code here
+        int sum=0;
+        for(int a:arr){
+            sum=sum+a;
+        }
+        return sum;
+    }
+}
