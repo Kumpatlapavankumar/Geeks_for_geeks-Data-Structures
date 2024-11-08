19e7db242983742a2298b88ad2c83f87534af4d6
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-11-08 22:46:19 +0530

    Create Count of smaller elements.java

---
+++ Count of smaller elements.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countOfElements(int x, List<Integer> arr) {
+        // Code here
+        int count = 0;
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            if (arr.get(i) <= x) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
