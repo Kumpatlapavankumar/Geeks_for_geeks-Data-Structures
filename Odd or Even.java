7da5e37c6ae6001ac9cd6a707000bc21bbe81c24
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-11-09 21:36:32 +0530

    Create Odd or Even.java

---
+++ Odd or Even.java
@@ -0,0 +1,12 @@
+
+// User function Template for Java
+class Solution {
+    static String oddEven(int n) {
+        // code here
+        if(n%2==0){
+            return "even";
+        }else{
+            return "odd";
+        }
+    }
+}
