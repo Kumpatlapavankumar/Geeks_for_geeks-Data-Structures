2ab54b740ca1b9441e48c582747852b3f3b353cc
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-11-12 21:46:24 +0530

    Create Bit Difference.java

---
+++ Bit Difference.java
@@ -0,0 +1,17 @@
+class Solution{
+    
+    // Function to find number of bits needed to be flipped to convert A to B
+    public static int countBitsFlip(int a, int b){
+        
+        // Your code here
+        int result=a^b;
+        int count=0;
+        while(result>0){
+            result=result&(result-1);
+            count++;
+        }
+        return count;
+    }
+    
+    
+}
