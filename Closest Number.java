6e03ea893caa4c77bf9f56bd0566f6eda533f543
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-11-15 21:42:58 +0530

    Create Closest Number.java

---
+++ Closest Number.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    static int closestNumber(int N , int M) {
+        // code here
+        int rem=N%M;
+        if(rem==0){
+            return N;
+        }
+        int i=N,j=N;
+        while(true){
+            if(i%M==0){
+                return i;
+            }else{
+                i--;
+            }
+            if(j%M==0){
+                return j;
+            }else{
+                j++;
+            }
+        }
+    }
+};
