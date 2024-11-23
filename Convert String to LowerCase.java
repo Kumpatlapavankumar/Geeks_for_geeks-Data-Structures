82d40a07cee2543bb80e4114e09214a2e329f3da
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-11-23 21:47:29 +0530

    Create Convert String to LowerCase.java

---
+++ Convert String to LowerCase.java
@@ -0,0 +1,8 @@
+// User function template for Java
+class Solution {
+    static String toLower(String s) {
+        // code here
+        String  result= s.toLowerCase();
+        return result;
+    }
+}
