cdf830beb9c8a0a905bd72a5bd400729429f6442
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-11-28 22:20:27 +0530

    Create Swap two numbers.java

---
+++ Swap two numbers.java
@@ -0,0 +1,10 @@
+class Solution{
+    static List<Integer> get(int a,int b)
+    {
+        // code here
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(b);
+        list.add(a);
+        return list;
+    }
+}
