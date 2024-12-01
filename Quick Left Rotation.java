a6afb8a2a082f1fffdd60006e46f0d153438b228
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-12-01 22:09:37 +0530

    Create Quick Left Rotation.java

---
+++ Quick Left Rotation.java
@@ -0,0 +1,18 @@
+class Solution {
+    void leftRotate(int arr[], int k) {
+        int n = arr.length;
+        k = k % n;
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
