b19ca95dc6eaafd3b310929aae407f0a9e959ea6
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-12-03 21:59:29 +0530

    Create Pattern 1.java

---
+++ Pattern 1.java
@@ -0,0 +1,11 @@
+class Solution {
+    void printSquare(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
