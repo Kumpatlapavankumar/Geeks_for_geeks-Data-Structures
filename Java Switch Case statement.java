c634c0489d9701c9762a61aad1772ea773bcf7c3
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-12-08 22:38:50 +0530

    Create Java Switch Case statement.java

---
+++ Java Switch Case statement.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+
+class Solution{
+    static double switchCase(int choice, List<Double> arr){
+        // code here
+        return (choice==1)?(Math.PI*arr.get(0)*arr.get(0)):arr.get(0)*arr.get(1);
+    }
+}
