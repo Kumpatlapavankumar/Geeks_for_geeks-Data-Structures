4cec62ed0d0c557fbbf333bd1b8c6cae8c4c689f
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-01-14 22:00:38 +0530

    Create House Robber.java

---
+++ House Robber.java
@@ -0,0 +1,12 @@
+//User function Template for Java
+
+class Solution {
+    static int maximizeMoney(int N , int K) {
+        // code here
+        int sum=0;
+        for(int i=1;i<=N;i=i+2){
+          sum=sum+K;  
+        }
+        return sum;
+    }
+};
