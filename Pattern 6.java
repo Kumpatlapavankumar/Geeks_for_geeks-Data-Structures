cb09e3263c16ec08d87ab616a24fab22663a2b66
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-01-20 22:06:02 +0530

    Create Pattern 6.java

---
+++ Pattern 6.java
@@ -0,0 +1,13 @@
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
