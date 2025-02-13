b0d7bf5379d54118d914e8bcfcc001b74866c201
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-02-13 22:15:26 +0530

    Create Pattern 7.java

---
+++ Pattern 7.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+         for(int i=1;i<=n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+         }
+    }
+}
