4e44d8e2b35d90a2ccecb3ffe2ed708f1ac856e0
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-03-01 22:08:56 +0530

    Create Check for Power.java

---
+++ Check for Power.java
@@ -0,0 +1,17 @@
+
+//User function Template for Java
+class Solution{
+    static Long isPowerOfAnother(Long X, Long Y){
+        // code here
+         if(Y==1){
+            return (long)1;
+        }
+        else if(X==1){
+            return (long)0;
+        }
+        if(Y%X!=0){
+            return (long)0;
+        }
+        return isPowerOfAnother(X,Y/X);
+    }
+}
