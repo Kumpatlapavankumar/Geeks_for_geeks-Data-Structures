5ec6b610bf829cc5795dbaf8713cd56f6d50725c
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-03-24 21:38:12 +0530

    Create Print With Space.java

---
+++ Print With Space.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+
+public static void utility() {
+    String a = scn.nextLine();
+    String b = scn.nextLine();
+    // Write your code below that prints a <space> b
+      System.out.println(a+" "+b);
+}
