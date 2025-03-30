c4768cbe62ea88ab3a9b8070a8801578fbc845e7
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-03-30 22:40:44 +0530

    Create   Lower case to upper case.java

---
+++ Lower case to upper case.java
@@ -0,0 +1,11 @@
+
+//User function Template for Java
+class Solution 
+{ 
+    String to_upper(String str) 
+    { 
+        // code here
+        String ans = str.toUpperCase();
+        return ans;
+    }
+} 
