b36555741f8b86e85ed03b121b6dc9568f1f7c38
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-04-02 22:12:51 +0530

    Create Switch Statement.java

---
+++ Switch Statement.java
@@ -0,0 +1,37 @@
+// User function Template for Java
+
+public static String utility(int number) {
+
+    // write your code here
+    switch(number){
+            case 1:
+                return "One";
+                
+            case 2: 
+                return "Two";
+                
+            case 3 :
+                return ("Three");
+                
+            case 4: 
+                return ("Four");
+                
+            case 5 :
+                return ("Five");
+                
+            case 6 :
+                return ("Six");
+                
+            case 7: 
+                return ("Seven");
+                
+            case 8 :
+                return ("Eight");
+                
+            case 9 :
+                return ("Nine");
+                
+            default:
+                return ("Unknown");
+    }
+}
