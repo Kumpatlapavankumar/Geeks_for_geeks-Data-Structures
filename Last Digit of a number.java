336d401e438848cae3c119bea41d4a7c93d74acc
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-04-07 22:48:39 +0530

    Create Last Digit of a number.java

---
+++ Last Digit of a number.java
@@ -0,0 +1,9 @@
+//User function Template for Java
+
+public static void utility(int n){
+
+    int ans = Math.abs(n % 10);;
+
+    //below statement print the result
+    System.out.println(ans);
+}
