252ac3340dff9859ea55343fc1a93bfe97f9b1e9
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-04-17 22:05:51 +0530

    Create Greatest of three numbers.java

---
+++ Greatest of three numbers.java
@@ -0,0 +1,11 @@
+
+// User function Template for Java
+
+class Solution {
+    int greatestOfThree(int A, int B, int C) {
+        // code here
+        if(A > B&& A>C) return A;
+        if(B> A&& B>C) return B;
+        else return C;
+    }
+}
