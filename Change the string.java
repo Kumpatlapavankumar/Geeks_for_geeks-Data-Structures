5d0cb21bbb97b227912d2325568f8c88a3d53ee2
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-04-19 21:07:40 +0530

    Create Change the string.java

---
+++ Change the string.java
@@ -0,0 +1,15 @@
+//User function Template for Java
+
+class Solution{
+    String modify(String s){
+        if(s.length()==0){
+            return "";
+        }
+        int n=(int)s.charAt(0);
+        if(n>=65&&n<=90){
+            return s.toUpperCase();
+        }else{
+            return s.toLowerCase();
+        }
+    }
+}
