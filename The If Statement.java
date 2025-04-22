0a6d54717fa9f615998eb3a310c0448679941336
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-04-22 22:15:52 +0530

    Create The If Statement.java

---
+++ The If Statement.java
@@ -0,0 +1,10 @@
+
+public static void utility(int number){
+    //Write your if statement below
+    if(number<100)
+    System.out.println("Number");
+    else{
+    System.out.println("Big");
+    System.out.println("Number");
+    }
+}
