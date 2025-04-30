808c8ca48935519a94b4d806bfcc989d0ee7b20d
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-04-30 22:17:37 +0530

    Create Deque Traversal.java

---
+++ Deque Traversal.java
@@ -0,0 +1,7 @@
+public static void printDeque(ArrayDeque<Integer> deq) {
+    // code here
+    for(Integer element : deq){
+        System.out.print(element +" ");
+    }
+    System.out.println();
+}
