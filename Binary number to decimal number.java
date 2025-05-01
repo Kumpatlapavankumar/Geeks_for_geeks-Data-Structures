489e27163b48467854f0c733b3ed0844e546b812
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-05-01 23:06:20 +0530

    Create  Binary number to decimal number.java

---
+++ Binary number to decimal number.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+
+class Solution {
+    public int binaryToDecimal(String b) {
+        // Code here
+         int x=Integer.parseInt(b,2);
+        return x;
+    }
+}
