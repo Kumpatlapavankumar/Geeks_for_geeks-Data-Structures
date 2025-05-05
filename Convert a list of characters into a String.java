69f2cde1ce8ce75d06252da7339b3477dd429682
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-05-05 23:27:14 +0530

    Create Convert a list of characters into a String.java

---
+++ Convert a list of characters into a String.java
@@ -0,0 +1,12 @@
+
+// User function Template for Java
+
+class Solution {
+    public String chartostr(char arr[], int N) {
+         String s="";
+      for(int i=0;i<arr.length;i++){
+          s+=arr[i];
+      }
+      return s;
+    }
+}
