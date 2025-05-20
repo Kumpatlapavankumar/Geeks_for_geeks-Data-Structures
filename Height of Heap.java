d65192158b02e95dcb26e06c96805d73760ebeee
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-05-20 22:37:30 +0530

    Create Height of Heap.java

---
+++ Height of Heap.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+
+class Solution {
+    static int heapHeight(int N, int arr[]) {
+        // code here
+        return (int)(Math.log(N) / Math.log(2));
+    }
+}
