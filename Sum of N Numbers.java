73659aa1787b5fe7bdf891603d24301999142aaa
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-05-22 22:23:25 +0530

    Create Sum of N Numbers.java

---
+++ Sum of N Numbers.java
@@ -0,0 +1,12 @@
+
+int nSum(int n) {
+    int ans = 0;
+
+    // Write your code here to calculate and return sum of n number.
+    for(int i =1; i<=n; i++){
+        ans+= i;
+        
+    }
+
+    return ans;
+}
