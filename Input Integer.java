1d63bdc3c459f6d338d50fc22d011517e92a65ac
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-05-28 21:31:21 +0530

    Create Input Integer.java

---
+++ Input Integer.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Implement Scanner Class
+        //  Take input
+        //  Add 10 and Print\ 
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int sum=n+10;
+        System.out.println(sum);
+    }
+}
