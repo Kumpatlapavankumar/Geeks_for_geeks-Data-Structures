f63bd3e4d5e501a9a3a262f31a4429f5d0a47dcf
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-06-02 22:15:50 +0530

    Create  Java Substring.java

---
+++ Java Substring.java
@@ -0,0 +1,9 @@
+// User function template for Java
+class Solution {
+    static String javaSub(String S, int L, int R) {
+        // code here
+        String result = S.substring(L, R + 1);
+        
+        return result;
+    }
+}
