7f6bc5bd775abfad4e2995ae23886f18bd618e51
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-06-04 22:14:13 +0530

    Create Divisor.java

---
+++ Divisor.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+class Solution {
+    public static void divisor(int n) {
+
+        // Write your code here
+            
+    for(int i=1;i<=n;i++){
+        if(n%i == 0){
+            System.out.print(i+ " ");
+        }
+    }
+    }
+}
