739784bf333474fa786cd3c006d04eb93fd6a518
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-06-07 17:07:21 +0530

    Create Table Difference.java

---
+++ Table Difference.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+class Solution {
+    public static void difference(int n1, int n2) {
+
+        // Write your code here
+        for(int i=1;i<=10;i++){
+            System.out.print(n1*i-n2*i+" ");
+        }
+    }
+}
