6e202c1f2f024a6be6370722875f46de6e42c9d8
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-06-14 22:11:51 +0530

    Create Evaluate Formulae.java

---
+++ Evaluate Formulae.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+class Solution {
+    public static void evalute(int a, int b, int c, int d) {
+        // Complete the code above that stores the output of the expression
+        int res = (a+b)/c + d;
+            // Complete the code above that stores the output of the expression
+
+            // This prints the output. Don't change it!
+            System.out.println(res);
+    }
+}
