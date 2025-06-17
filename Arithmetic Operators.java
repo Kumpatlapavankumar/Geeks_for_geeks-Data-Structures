3caf1dad9364cebcc58c1aefcb1d5017c3744412
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-06-17 22:06:45 +0530

    Create Arithmetic Operators.java

---
+++ Arithmetic Operators.java
@@ -0,0 +1,19 @@
+// User function Template for Java
+class Solution {
+
+    public static void operations(int x, int y) {
+        // Perform addition x+y below
+        int p =x+y;
+            // Perform subtraction x-y below
+            int q =x-y;
+                // Perform multiplication x*y below
+            int r =x*y;
+                // Perform division x/y below
+            int s =x/y;
+                // Perform modulo operation x%y below
+            int t =x%y;
+
+                // The below code prints the output. Don't change it!
+            System.out.println(p + " " + q + " " + r + " " + s + " " + t);
+    }
+}
