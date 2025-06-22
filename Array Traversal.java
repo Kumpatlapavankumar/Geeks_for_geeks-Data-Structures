8521b15e69ba2a4eb5a8055f2854315cdc135285
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-06-22 21:29:09 +0530

    Create Array Traversal.java

---
+++ Array Traversal.java
@@ -0,0 +1,8 @@
+class Solution {
+    public static void arrayTraversal(int[] arr) {
+        // Code here
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
