b7891cd6607b8201fc2dfe4014e6046b42affe8e
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-06-29 21:46:05 +0530

    Create Arithmetic Operators efficient.java

---
+++ Arithmetic Operators efficient.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] computeOperations(int x, int y) {
+        // code here
+        int p = x + y; 
+        int q = x - y;  
+        int r = x * y;
+        int s = x / y; 
+        int t = x % y;
+
+        return new int[]{p, q, r, s, t};
+    }
+}
